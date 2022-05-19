875c33a400a3bab165127ec1fe9ef7783e0bc5d8
corbatsh <bortlikkamil@gmail.com>
2022-05-19 11:39:22 +0200

    login e2e test with page object model

---
+++ cypress/integration/e2e/login.spec.js
@@ -0,0 +1,29 @@
+import { login_password, login_username, url } from '../../../config'
+import Navbar from '../../pages-objects/components/Navbar'
+import loginPage from '../../pages-objects/pages/loginPage'
+
+describe('Login Failed Test', () => {
+	before(() => {
+		cy.visit(url)
+		Navbar.clickSignIn()
+	})
+
+	it('Should try to login with invalid credentials', () => {
+		loginPage.login('invalid username', 'invalid password')
+	})
+
+	it('Should display an error message', () => {
+		loginPage.displayErrorMessage()
+	})
+})
+
+describe('Login Success Test', () => {
+	before(() => {
+		cy.visit(url)
+		Navbar.clickSignIn()
+	})
+
+	it('Should login into application', () => {
+		loginPage.login(login_username, login_password)
+	})
+})
